5d3061c4e75410f776a00ac996ea338b950fbe02
zil130 <55101149+zil130@users.noreply.github.com>
2023-06-21 01:53:30 -0300

    init: start task

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>initial state of app</h1>
+</template>
+
+<script>
+
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
